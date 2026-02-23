4a110fab258a97181501e835a867d62ddec39bfa
HCM281994 <h.alexis.cid18@gmail.com>
2026-02-23 19:37:57 -0300

    Feature: Añadido modo aprendizaje y guardado en EEPROM

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,57 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 #include "lector_rfid.h"
 
-// ID de tu tarjeta Maestra
 const String MASTER_UID = "5B6E884F";
+String tarjetaAutorizada;
 
 void setup() {
     Serial.begin(9600);
     inicializarSistema();
-    Serial.println("SISTEMA DE SEGURIDAD ACTIVADO");
-    Serial.println("Esperando tarjeta maestra...");
+    
+    // Leer la tarjeta guardada en EEPROM (si existe)
+    char datos[9];
+    for (int i = 0; i < 8; i++) {
+        datos[i] = EEPROM.read(i);
+    }
+    datos[8] = '\0';
+    tarjetaAutorizada = String(datos);
+    
+    Serial.println("Sistema Listo. Master: " + MASTER_UID);
+    Serial.println("Autorizada actual: " + tarjetaAutorizada);
 }
 
 void loop() {
     String uid = obtenerUID();
     
     if (uid != "") {
-        Serial.print("UID Detectado: ");
-        Serial.println(uid);
-        
         if (uid == MASTER_UID) {
-            Serial.println("ACCESO CONCEDIDO - Bienvenida, Master Card.");
-            tone(BUZZER_PIN, 2000, 500); // Pitido largo de éxito
+            Serial.println("¡Modo Programación Activado! Pase la nueva tarjeta...");
+            tone(BUZZER_PIN, 1500, 1000); // Pitido largo de aviso
             
+            // Esperar a la nueva tarjeta
+            String nuevaUID = "";
+            while(nuevaUID == "") {
+                nuevaUID = obtenerUID();
+                delay(100);
+            }
+            
+            // Guardar en EEPROM
+            for (int i = 0; i < 8; i++) {
+                EEPROM.write(i, nuevaUID[i]);
+            }
+            tarjetaAutorizada = nuevaUID;
+            Serial.println("Nueva tarjeta guardada: " + nuevaUID);
+            tone(BUZZER_PIN, 2000, 500);
+            
+        } else if (uid == tarjetaAutorizada) {
+            Serial.println("Acceso Concedido");
             digitalWrite(RELAY_PIN, HIGH);
-            delay(3000); // Mantenemos el relay activo 3 segundos
+            delay(3000);
             digitalWrite(RELAY_PIN, LOW);
         } else {
-            Serial.println("ACCESO DENEGADO - Tarjeta no autorizada.");
-            // Pitidos cortos de error
-            for(int i=0; i<3; i++){
-                tone(BUZZER_PIN, 500, 100);
-                delay(150);
-            }
+            Serial.println("Acceso Denegado");
+            tone(BUZZER_PIN, 400, 500);
         }
     }
 }
